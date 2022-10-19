2d260dd8a3662b4942c66b8a4664e2007ac06a38
Bram Cohen <bram@bramcohen.com>
2022-10-19 14:33:08 -0700

    Added in referee and accusations and tweaked names

--- testrockpaperscissors.py
+++ testrockpaperscissors.py
@@ -3,10 +3,21 @@ import hashlib
 from hsms.streamables.program import Program
 from hsms.puzzles.load_clvm import load_clvm
 
+from clvm.EvalError import EvalError
+
+
 MOD_A = Program.from_bytes(bytes.fromhex(open("rockpaperscissorsa.clvm.hex").read()))
 MOD_B = Program.from_bytes(bytes.fromhex(open("rockpaperscissorsb.clvm.hex").read()))
 MOD_C = Program.from_bytes(bytes.fromhex(open("rockpaperscissorsc.clvm.hex").read()))
 MOD_D = Program.from_bytes(bytes.fromhex(open("rockpaperscissorsd.clvm.hex").read()))
+
+
+def drun(prog: Program, *args: Program):
+    try:
+        return prog.run(*args)
+    except EvalError as ee:
+        print(f"brun -x -y main.sym {prog} {Program.to(list(args))}")
+        raise
 
 def sha256(blob:bytes) -> bytes:
     return hashlib.sha256(blob).digest()
@@ -21,12 +32,12 @@ def testrps(amove, bmove):
     alice_move = Program.to(amove)
 
     cd = MOD_D.curry(alice_move, bob_image)
-    assert cd.run([total, b'', alice_final, bob_preimage]).atom == b''
+    assert cd.run([total, bob_preimage, b'', alice_final]).atom == b''
     cc = MOD_C.curry(alice_image, bob_image)
-    assert cc.run([total, cd.tree_hash(), 0, alice_preimage]).atom == b''
+    assert drun(cc, [total, alice_preimage, cd.tree_hash(), 0]).atom == b''
     cb = MOD_B.curry(alice_image)
-    assert cb.run([total, cc.tree_hash(), 0, bob_image]).atom == b''
-    assert MOD_A.run([total, cb.tree_hash(), 0, alice_image]).atom == b''
+    assert cb.run([total, bob_image, cc.tree_hash(), 0]).atom == b''
+    assert MOD_A.run([total, alice_image, cb.tree_hash(), 0]).atom == b''
 
 def testall():
     for i in range(3):
