ce15f48f622a8892845f1800aa7dc6e3a4a2bba7
arty <art.yerkes@gmail.com>
2023-03-24 10:17:00 -0700

    move sort back closer to bram's original with fixes and speed up test run

--- test_library_basics.py
+++ test_library_basics.py
@@ -10,16 +10,16 @@ compile_module_with_symbols(['.'], 'smoke_test_sort.clsp')
 sort_program = Program.from_bytes(bytes.fromhex(open('smoke_test_sort.clvm.hex').read()))
 
 def test_smoke_compare():
-    diag_run_clvm(compare_program, Program.to([]), 'smoke_test_deep_compare.sym')
+    compare_program.run(Program.to([]))
 
 def test_smoke_sort():
-    for length in range(11): # 0-10 length
+    for length in range(10): # 0-10 length
         for i in range(1 + (3 * length)): # A few orders each
             orig_list = [random.randint(0,100) for x in range(length)]
             sort_args = Program.to([orig_list])
             sorted_list = Program.to(sorted(orig_list))
             print(orig_list)
-            sort_res = diag_run_clvm(sort_program, sort_args, 'smoke_test_sort.sym')
+            sort_res = sort_program.run(sort_args)
             print(sort_res,sorted_list)
             assert sort_res == sorted_list
 
