9ba9222235f1730e818a94a913a91878a5ad5667
arty <art.yerkes@gmail.com>
2023-10-13 14:58:33 -0700

    Add test content

--- backend/src/lib/tests/index.test.ts
+++ backend/src/lib/tests/index.test.ts
@@ -6,9 +6,24 @@ const {h, t, Program, compile} = require('../../../clvm_tools_rs/wasm/pkg/clvm_t
 
 it('can compile clvm', async () => {
     const program_output = compile(
-        '(mod (X) (include *standard-cl-23*) (+ X 1))',
-        'test.clsp',
-        []
+        fs.readFileSync('test-content/t1.clsp', 'utf8'),
+        't1.clsp',
+        ['test-content'],
+        {
+            "read_new_file": (filename: string, dirs: Array<string>) => {
+                for (let d in dirs) {
+                    let dir = dirs[d];
+                    let path = resolve(dir, filename);
+                    try {
+                        return fs.readFileSync(path, 'utf8');
+                    } catch (e) {
+                        // Ok, try the next dir.
+                    }
+                }
+
+                throw `Could not find file ${filename}`;
+            }
+        }
     );
     assert.equal(program_output.hex, 'ff10ff02ffff010180');
 });
