690710225b60976f3ff2390300f0a771980eda91
arty <art.yerkes@gmail.com>
2023-10-13 15:18:50 -0700

    Add symbol output

--- backend/src/cmdline/index.ts
+++ backend/src/cmdline/index.ts
@@ -1,7 +1,7 @@
 import * as process from 'process';
 import { program } from 'commander';
 import * as fs from 'fs';
-import { resolve } from 'path';
+import * as path from 'path';
 const {compile} = require('../clvm_tools_rs/wasm/pkg/clvm_tools_wasm');
 
 function collect<A>(value: A, previous: Array<A>): Array<A> {
@@ -11,6 +11,7 @@ function collect<A>(value: A, previous: Array<A>): Array<A> {
 program
     .description('simple chialisp compiler interface')
     .argument('<chialisp program>', 'program to compile')
+    .option('-s, --symbols <output sym file>', 'symbol file')
     .option('-o, --output <output hex file>', 'hex output')
     .option('-i, --include <path ...>', 'add search path', collect, []);
 
@@ -38,6 +39,11 @@ try {
     process.exit(1);
 }
 
+let symbol_file = opts.symbols;
+if (!symbol_file) {
+    symbol_file = path.format({ ...path.parse(opts.output), base: '', ext: '.sym' });
+}
+
 try {
     let program_output = compile(
         input_program,
@@ -47,9 +53,9 @@ try {
             "read_new_file": (filename: string, dirs: Array<string>) => {
                 for (let d in dirs) {
                     let dir = dirs[d];
-                    let path = resolve(dir, filename);
+                    let read_path = path.resolve(dir, filename);
                     try {
-                        return fs.readFileSync(path, 'utf8');
+                        return fs.readFileSync(read_path, 'utf8');
                     } catch (e) {
                         // Ok, try the next dir.
                     }
@@ -66,6 +72,7 @@ try {
     }
 
     fs.writeFileSync(opts.output, program_output.hex);
+    fs.writeFileSync(symbol_file, JSON.stringify(program_output.symbols));
 } catch (e) {
     console.error(e);
     process.exit(1);
