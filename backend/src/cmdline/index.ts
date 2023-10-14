ef702eae1acfa19c7120e674ed1dcc363907c4bc
arty <art.yerkes@gmail.com>
2023-10-13 23:28:56 -0700

    Fix up compilation to take dependency dates into account

--- backend/src/cmdline/index.ts
+++ backend/src/cmdline/index.ts
@@ -2,11 +2,36 @@ import * as process from 'process';
 import { program } from 'commander';
 import * as fs from 'fs';
 import * as path from 'path';
-const {compile} = require('../clvm_tools_rs/wasm/pkg/clvm_tools_wasm');
+const {compile, get_dependencies} = require('../clvm_tools_rs/wasm/pkg/clvm_tools_wasm');
 
 function collect<A>(value: A, previous: Array<A>): Array<A> {
     return previous.concat([value]);
 }
+
+function get_file_date(filename: string): number|undefined {
+    try {
+        let stat = fs.lstatSync(filename);
+        return stat.mtimeMs;
+    } catch (e) {
+        return undefined;
+    }
+}
+
+const compiler_options = {
+    "read_new_file": (filename: string, dirs: Array<string>) => {
+        for (let d in dirs) {
+            let dir = dirs[d];
+            let read_path = path.resolve(dir, filename);
+            try {
+                return [read_path, fs.readFileSync(read_path, 'utf8')];
+            } catch (e) {
+                // Ok, try the next dir.
+            }
+        }
+
+        throw `Could not find file ${filename}`;
+    }
+};
 
 program
     .description('simple chialisp compiler interface')
@@ -45,34 +70,50 @@ if (!symbol_file) {
 }
 
 try {
-    let program_output = compile(
+    let dependencies = get_dependencies(
         input_program,
         input_file,
         opts.include,
-        {
-            "read_new_file": (filename: string, dirs: Array<string>) => {
-                for (let d in dirs) {
-                    let dir = dirs[d];
-                    let read_path = path.resolve(dir, filename);
-                    try {
-                        return fs.readFileSync(read_path, 'utf8');
-                    } catch (e) {
-                        // Ok, try the next dir.
-                    }
-                }
-
-                throw `Could not find file ${filename}`;
-            }
-        }
+        compiler_options
     );
 
-    if (program_output.error) {
-        console.error(program_output.error);
+    if (dependencies.error) {
+        console.error(dependencies.error);
         process.exit(1);
     }
 
-    fs.writeFileSync(opts.output, program_output.hex);
-    fs.writeFileSync(symbol_file, JSON.stringify(program_output.symbols));
+    let outputTime = get_file_date(opts.output);
+    let outputIsNewer;
+
+    if (outputTime) {
+        outputIsNewer = get_file_date(input_file) < outputTime;
+
+        for (let d in dependencies) {
+            const dep = dependencies[d];
+            if (get_file_date(dep) > outputTime) {
+                outputIsNewer = false;
+            }
+        }
+    } else {
+        outputIsNewer = false;
+    }
+
+    if (!outputIsNewer) {
+        let program_output = compile(
+            input_program,
+            input_file,
+            opts.include,
+            compiler_options
+        );
+
+        if (program_output.error) {
+            console.error(program_output.error);
+            process.exit(1);
+        }
+
+        fs.writeFileSync(opts.output, program_output.hex);
+        fs.writeFileSync(symbol_file, JSON.stringify(program_output.symbols));
+    }
 } catch (e) {
     console.error(e);
     process.exit(1);
