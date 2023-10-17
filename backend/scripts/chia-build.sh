86fb6c7eef08d07ea41e90eb90b0bf0bb76a3699
arty <art.yerkes@gmail.com>
2023-10-17 12:17:04 -0700

    Add a full handcalc test in javascript

--- backend/scripts/chia-build.sh
+++ backend/scripts/chia-build.sh
@@ -4,5 +4,8 @@ set -e
 
 THISDIR=$(dirname "$0")
 
-# Compile test_handcalc.clsp
-./scripts/clspc.sh -i .. -o test_handcalc.hex ../test_handcalc.clsp
+mkdir -p clvm-hex
+./scripts/clspc.sh -i .. -o clvm-hex/test_handcalc.hex ../test_handcalc.clsp
+./scripts/clspc.sh -i .. -o clvm-hex/run_handcalc.hex test-content/run_handcalc.clsp
+./scripts/clspc.sh -i .. -o clvm-hex/run_onehandcalc.hex test-content/run_onehandcalc.clsp
+
--- backend/src/lib/tests/index.test.ts
+++ backend/src/lib/tests/index.test.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import * as assert from 'assert';
 import * as bls_loader from 'bls-signatures';
 const {h, t, Program, compile} = require('../../../clvm_tools_rs/wasm/pkg/clvm_tools_wasm');
+import {Hand} from 'pokersolver';
 
 it('can compile clvm', async () => {
     const program_output = compile(
@@ -35,10 +36,18 @@ class Card {
         this.value = value;
     }
 
+    rank(): number {
+        return (this.value / 4) + 1;
+    }
+
+    suit(): number {
+        return (this.value % 4) + 1;
+    }
+
     toString(): string {
         const rank = this.value / 4;
         const suit = this.value % 4;
-        return "HDCS".charAt(suit) + "A23456789TJQK".charAt(rank);
+        return "A23456789TJQK".charAt(rank) + "hdcs".charAt(suit);
     }
 }
 
@@ -68,7 +77,60 @@ class CardDeck {
 }
 
 it('can try playing poker hands', async () => {
+    // Make some cards
     const deck = new CardDeck(99, 3, 17);
     const cards = deck.deal(7);
-    assert.equal(cards.join(','), 'D5,C5,D6,CT,H9,H5,S3');
+    assert.equal(cards.join(','), '5d,5c,6d,Tc,9h,5h,3s');
+
+    // Load program.
+    const run_handcalc_hex = fs.readFileSync('clvm-hex/run_handcalc.hex', 'utf8');
+    const run_handcalc = Program.from_hex(run_handcalc_hex);
+    const run_onehandcalc_hex = fs.readFileSync('clvm-hex/run_onehandcalc.hex', 'utf8');
+    const run_onehandcalc = Program.from_hex(run_onehandcalc_hex);
+
+    function cards_from_bitmap(cards, bitmap) {
+        const chosen_cards = [];
+        for (var idx = 0; idx < cards.length; idx++) {
+            if (bitmap & (1 << idx)) {
+                chosen_cards.push(cards[idx]);
+            }
+        }
+        return chosen_cards;
+    };
+
+    // Form argument to handcalc.
+    const cards_clvm = Program.to([cards.map((c) => t(c.rank(), c.suit()))]);
+    let chosen_cards = [];
+    try {
+        // Run handcalc.  The result is a bitmap on the cards.
+        const [cost, result] = run_handcalc.run(cards_clvm);
+        chosen_cards = cards_from_bitmap(cards, result.as_int());
+    } catch (e) {
+        console.error(e.toString());
+        assert.equal(e, null);
+    }
+
+    // Three of a kind hand.
+    const test_cards_clvm = Program.to([chosen_cards.map((c) => t(c.rank(), c.suit()))]);
+
+    let hand_type = null;
+    try {
+        // Run onehandcalc.  The result is a description of a hand.
+        const [cost, result] = run_onehandcalc.run(test_cards_clvm);
+        hand_type = result.as_javascript();
+    } catch (e) {
+        console.error(e.toString());
+        assert.equal(e, null);
+    }
+
+    // Show it's a 3 of a kind hand.
+    assert.equal(hand_type[0], 3);
+    assert.equal(hand_type[1], 1);
+    assert.equal(hand_type[2], 1);
+
+    // Show the card.
+    assert.equal(hand_type[3], 5);
+
+    const poker_hand = Hand.solve(cards.map((c) => c.toString()));
+    assert.equal(poker_hand.descr, "Three of a Kind, 5's");
 });
