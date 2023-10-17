530a51ad9775d3c977ef20fb6b063e5d23b0be6a
arty <art.yerkes@gmail.com>
2023-10-17 13:44:59 -0700

    Make a nice object that allows us to pass in card hands and test

--- backend/src/lib/tests/index.test.ts
+++ backend/src/lib/tests/index.test.ts
@@ -29,6 +29,22 @@ it('can compile clvm', async () => {
     assert.equal(program_output.hex, 'ff10ff02ffff010180');
 });
 
+function intify(n) {
+    return Number(n);
+}
+
+function string_of_rank(rank) {
+    return " A23456789TJQK".charAt(intify(rank));
+}
+
+function poker_lib_string_of_rank(rank) {
+    if (rank == 10) {
+        return "10";
+    } else {
+        return string_of_rank(rank);
+    }
+}
+
 class Card {
     value: number
 
@@ -47,7 +63,7 @@ class Card {
     toString(): string {
         const rank = this.value / 4;
         const suit = this.value % 4;
-        return "A23456789TJQK".charAt(rank) + "hdcs".charAt(suit);
+        return string_of_rank(rank + 1) + "hdcs".charAt(suit);
     }
 }
 
@@ -76,19 +92,48 @@ class CardDeck {
     }
 }
 
-it('can try playing poker hands', async () => {
-    // Make some cards
-    const deck = new CardDeck(99, 3, 17);
-    const cards = deck.deal(7);
-    assert.equal(cards.join(','), '5d,5c,6d,Tc,9h,5h,3s');
+class HandcalcTestRig {
+    run_handcalc_program: IProgram
+    run_onehandcalc_program: IProgram
 
-    // Load program.
-    const run_handcalc_hex = fs.readFileSync('clvm-hex/run_handcalc.hex', 'utf8');
-    const run_handcalc = Program.from_hex(run_handcalc_hex);
-    const run_onehandcalc_hex = fs.readFileSync('clvm-hex/run_onehandcalc.hex', 'utf8');
-    const run_onehandcalc = Program.from_hex(run_onehandcalc_hex);
+    constructor() {
+        // Load programs.
+        const run_handcalc_hex = fs.readFileSync('clvm-hex/run_handcalc.hex', 'utf8');
+        this.run_handcalc_program = Program.from_hex(run_handcalc_hex);
+        const run_onehandcalc_hex = fs.readFileSync('clvm-hex/run_onehandcalc.hex', 'utf8');
+        this.run_onehandcalc_program = Program.from_hex(run_onehandcalc_hex);
+    }
 
-    function cards_from_bitmap(cards, bitmap) {
+    hand_description_from_onehandcalc(ohc_output) {
+        if (ohc_output[0] == 5) {
+            // Straight flush
+            return `Straight Flush, ${poker_lib_string_of_rank(ohc_output[1])}s High`;
+        } else if (ohc_output[0] == 4) {
+            // Four of a kind
+            return `Four of a Kind, ${poker_lib_string_of_rank(ohc_output[2])}'s`;
+        } else if (ohc_output[0] == 3) {
+            if (ohc_output[1] == 1 && ohc_output[2] == 3) {
+                // Flush
+                return `Flush, ${poker_lib_string_of_rank(ohc_output[3])}s High`;
+            } else if (ohc_output[1] == 1 && ohc_output[2] == 2) {
+                return `Straight, ${poker_lib_string_of_rank(ohc_output[3])} High`;
+            } else if (ohc_output[1] == 1 && ohc_output[2] == 1) {
+                // Three of a kind
+                return `Three of a Kind, ${poker_lib_string_of_rank(ohc_output[3])}'s`;
+            } else if (ohc_output[1] == 2) {
+                // Full house
+                return `Full House, ${poker_lib_string_of_rank(ohc_output(3))}'s over ${string_of_rank(ohc_output(4))}'s`;
+            }
+        } else if (ohc_output[0] == 2) {
+            // Pair
+            return `Pair, ${poker_lib_string_of_rank(ohc_output[5])}`;
+        } else {
+            // High card
+            return `${poker_lib_string_of_rank(ohc_output[5])} High`;
+        }
+    }
+
+    cards_from_bitmap(cards, bitmap) {
         const chosen_cards = [];
         for (var idx = 0; idx < cards.length; idx++) {
             if (bitmap & (1 << idx)) {
@@ -96,41 +141,56 @@ it('can try playing poker hands', async () => {
             }
         }
         return chosen_cards;
-    };
+    }
 
-    // Form argument to handcalc.
-    const cards_clvm = Program.to([cards.map((c) => t(c.rank(), c.suit()))]);
-    let chosen_cards = [];
-    try {
+    run_handcalc(cards) {
+        // Form argument to handcalc.
+        const cards_clvm = Program.to([cards.map((c) => t(c.rank(), c.suit()))]);
+        let chosen_cards = [];
+
         // Run handcalc.  The result is a bitmap on the cards.
-        const [cost, result] = run_handcalc.run(cards_clvm);
-        chosen_cards = cards_from_bitmap(cards, result.as_int());
-    } catch (e) {
-        console.error(e.toString());
-        assert.equal(e, null);
+        const [cost, result] = this.run_handcalc_program.run(cards_clvm);
+        return this.cards_from_bitmap(cards, result.as_int());
     }
 
-    // Three of a kind hand.
-    const test_cards_clvm = Program.to([chosen_cards.map((c) => t(c.rank(), c.suit()))]);
-
-    let hand_type = null;
-    try {
+    run_onehandcalc(cards) {
         // Run onehandcalc.  The result is a description of a hand.
-        const [cost, result] = run_onehandcalc.run(test_cards_clvm);
-        hand_type = result.as_javascript();
+        const test_cards_clvm = Program.to([cards.map((c) => t(c.rank(), c.suit()))]);
+        const [cost, result] = this.run_onehandcalc_program.run(test_cards_clvm);
+        return result.as_javascript();
+    }
+
+    run_card_hand_test(cards) {
+        // Three of a kind hand.
+        const chosen_cards = this.run_handcalc(cards);
+        const classified = this.run_onehandcalc(chosen_cards);
+        const hand = Hand.solve(chosen_cards.map((c) => c.toString()));
+
+        assert.equal(this.hand_description_from_onehandcalc(classified), hand.descr);
+
+        return {
+            'hand': hand,
+            'chosen': chosen_cards,
+            'classified': classified
+        };
+    }
+}
+
+it('can try playing poker hands', async () => {
+    // Make some cards
+    const deck = new CardDeck(99, 3, 17);
+    const cards = deck.deal(7);
+    assert.equal(cards.join(','), '5d,5c,6d,Tc,9h,5h,3s');
+
+    const test_rig = new HandcalcTestRig();
+
+    let cards_result = null;
+    try {
+        cards_result = test_rig.run_card_hand_test(cards);
     } catch (e) {
         console.error(e.toString());
         assert.equal(e, null);
     }
 
-    // Show it's a 3 of a kind hand.
-    assert.equal(hand_type[0], 3);
-    assert.equal(hand_type[1], 1);
-    assert.equal(hand_type[2], 1);
-
-    // Show the card.
-    assert.equal(hand_type[3], 5);
-
-    const poker_hand = Hand.solve(cards.map((c) => c.toString()));
-    assert.equal(poker_hand.descr, "Three of a Kind, 5's");
+    assert.equal(cards_result.hand.descr, "Three of a Kind, 5's");
 });
