606b915a03e0208693a199609fb09599a8edc4dc
arty <art.yerkes@gmail.com>
2024-05-15 15:46:16 -0700

    Should be strictly better.  Allow us to use the debug game program properly as the validation program

--- src/tests/referee.rs
+++ src/tests/referee.rs
@@ -8,7 +8,7 @@ use clvm_tools_rs::classic::clvm_tools::binutils::{assemble, disassemble};
 
 use crate::channel_handler::game_handler::GameHandler;
 use crate::channel_handler::types::{GameStartInfo, ReadableMove};
-use crate::common::standard_coin::read_hex_puzzle;
+use crate::common::standard_coin::{private_to_public_key, read_hex_puzzle, puzzle_hash_for_pk};
 use crate::common::types::{
     Aggsig, AllocEncoder, Amount, Error, GameID, Node, PrivateKey, PuzzleHash, Sha256tree, Timeout,
 };
@@ -45,21 +45,44 @@ fn test_referee_smoke() {
     let seed: [u8; 32] = [0; 32];
     let mut rng = ChaCha8Rng::from_seed(seed);
     let mut allocator = AllocEncoder::new();
-    let referee_coin_puzzle_hash: PuzzleHash = rng.gen();
-    let private_key: PrivateKey = rng.gen();
-    let their_puzzle_hash: PuzzleHash = rng.gen();
-    let nil = allocator.allocator().null();
-    let val_hash = Node(nil).sha256tree(&mut allocator);
-    let init_state = assemble(allocator.allocator(), "(0 . 0)").expect("should assemble");
+
+    // Load up the real referee coin.
+    let referee_coin_puzzle = read_hex_puzzle(&mut allocator, "onchain/referee.hex").expect("should be readable");
+    let referee_coin_puzzle_hash: PuzzleHash = referee_coin_puzzle.sha256tree(&mut allocator);
+
+    // Generate keys and puzzle hashes.
+    let my_private_key: PrivateKey = rng.gen();
+    let my_public_key = private_to_public_key(&my_private_key);
+    let my_puzzle_hash = puzzle_hash_for_pk(&mut allocator, &my_public_key).expect("should have");
+
+    let their_private_key: PrivateKey = rng.gen();
+    let their_public_key = private_to_public_key(&their_private_key);
+    let their_puzzle_hash: PuzzleHash = puzzle_hash_for_pk(&mut allocator, &their_public_key).expect("should have");
+
+    let amount = Amount::new(100);
+
+    let debug_game_handler = make_debug_game_handler(&mut allocator, &amount);
+    let validation_program = CurriedProgram {
+        program: Node(debug_game_handler.to_nodeptr()),
+        args: clvm_curried_args!(1337)
+    }.to_clvm(&mut allocator).expect("should curry");
+    let init_state =
+        assemble(
+            allocator.allocator(),
+            "(0 . 0)"
+        ).expect("should assemble");
+
+    let validation_program_hash = Node(validation_program).sha256tree(&mut allocator);
+
     let amount = Amount::new(100);
     let game_start_info = GameStartInfo {
         game_id: GameID::from_bytes(b"test"),
         amount: amount.clone(),
-        game_handler: make_debug_game_handler(&mut allocator, &amount),
+        game_handler: debug_game_handler,
         timeout: Timeout::new(1000),
         is_my_turn: true,
-        initial_validation_puzzle: nil,
-        initial_validation_puzzle_hash: val_hash,
+        initial_validation_puzzle: validation_program,
+        initial_validation_puzzle_hash: validation_program_hash,
         initial_state: init_state,
         initial_move: vec![0, 0],
         initial_max_move_size: 0,
@@ -67,9 +90,9 @@ fn test_referee_smoke() {
     };
     let mut referee = RefereeMaker::new(
         &mut allocator,
-        referee_coin_puzzle_hash,
+        referee_coin_puzzle_hash.clone(),
         &game_start_info,
-        &private_key,
+        &my_private_key,
         &their_puzzle_hash,
         1,
     )
@@ -100,10 +123,12 @@ fn test_referee_smoke() {
         assert!(false);
     }
 
+    let on_chain_cheat = referee.clone();
+
     let their_move_result = referee
         .their_turn_move_off_chain(
             &mut allocator,
-            &[0],
+            &[],
             &my_move_result.validation_info_hash,
             100,
             &Amount::default(),
@@ -113,5 +138,14 @@ fn test_referee_smoke() {
     assert_eq!(
         disassemble(allocator.allocator(), their_move_result.readable_move, None),
         "(())"
+    );
+
+    let mut their_referee = RefereeMaker::new(
+        &mut allocator,
+        referee_coin_puzzle_hash,
+        &game_start_info,
+        &their_private_key,
+        &their_puzzle_hash,
+        1,
     );
 }
