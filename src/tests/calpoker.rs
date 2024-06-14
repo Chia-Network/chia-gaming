20c46aa1d694ad91e5026e868e472e49e6ebaec1
arty <art.yerkes@gmail.com>
2024-06-14 10:23:49 -0700

    Reverse polarity

--- src/tests/calpoker.rs
+++ src/tests/calpoker.rs
@@ -26,3 +26,27 @@ fn test_load_calpoker() {
         &contributions
     ).expect("should get a sim env");
 }
+
+// #[test]
+// fn test_play_calpoker() {
+//     let mut allocator = AllocEncoder::new();
+//     let seed: [u8; 32] = [0; 32];
+//     let mut rng = ChaCha8Rng::from_seed(seed);
+//     let calpoker = load_calpoker(&mut allocator).expect("should load");
+//     let contributions = [Amount::new(100), Amount::new(100)];
+
+//     let mut simenv = SimulatorEnvironment::new(
+//         &mut allocator,
+//         &mut rng,
+//         &calpoker,
+//         &contributions
+//     ).expect("should get a sim env");
+
+//     let alice_word = b"0alice6789abcdef";
+//     let alice_word_hash = Sha256Input::Bytes(alice_word).hash();
+//     let moves = [
+//         GameAction::Move(0, alice_word_hash.to_clvm(simenv.env.allocator).expect("should convert")),
+//     ];
+//     let play_result = simenv.play_game(&moves);
+//     todo!();
+// }
