4452a1315755a9a8a63954bed5a4cf280a145897
arty <art.yerkes@gmail.com>
2024-07-11 17:27:21 -0700

    Fix compile error

--- src/outside.rs
+++ src/outside.rs
@@ -39,24 +39,105 @@ struct GameInfoTheirTurn {
 }
 
 /// Async interface for messaging out of the game layer toward the wallet.
+///
+/// For this and its companion if instances are left in the documentation which
+/// refer to the potato handler combining spend bundles, that work has been decided
+/// to not take place in the potato handler.  The injected wallet bootstrap
+/// dependency must be stateful enough that it can cope with receiving a partly
+/// funded offer spend bundle and fully fund it if needed.
 pub trait BootstrapTowardGame {
-    /// Tells the game layer that we received a partly signed channel offer.
-    fn received_channel_offer(&mut self, bundle: &TransactionBundle) -> Result<(), Error>;
-    /// Tells the game layer that the fully signed transaction to make the
-    /// channel coin has come through.
-    fn received_channel_transaction_completion(&mut self, bundle: &TransactionBundle) -> Result<(), Error>;
+    /// Gives a partly signed offer to the wallet bootstrap.
+    ///
+    /// Intended use: channel_puzzle_hash delivers the desired puzzle hash and this
+    /// is the reply which delivers a transaction bundle for an already spent
+    /// transaction creating the channel coin.
+    ///
+    /// The launcher program is passed a list of conditions and returns that list
+    /// of conditions with an announcement including their shatree as an
+    /// announcement.
+    ///
+    /// The launcher coin is implicit in the returned transaction bundle in that
+    /// we can compute its coin string from this information.
+    ///
+    /// The launcher coin must be a specific program such as the singleton
+    /// launcher.
+    ///
+    /// The launcher coin targets the channel puzzle with the right amount.
+    ///
+    /// "Half funded" transaction in a spend bundle to which spends will be
+    /// added that fully fund it, condition on the given announcement named
+    /// above by the launcher coin.
+    ///
+    /// The launcher coin will be in here so the other guy can pick it out and
+    /// make the assumption that it is the launcher coin.  It is identifiable by
+    /// its puzzle hash.
+    ///
+    /// We forward this spend bundle over a potato message and the peer passes
+    /// it to the other guy's injected wallet dependency via received_channel_offer
+    /// below.
+    ///
+    /// channel offer should deliver both the launcher coin id and the partly
+    /// funded spend bundle.  Alice absolutely needs the channel coin id in some
+    /// way from here.
+    ///
+    /// Only alice sends this spend bundle in message E, but only after receiving
+    /// message D.
+    fn channel_offer(&mut self, bundle: &SpendBundle) -> Result<(), Error>;
+
+    /// Gives the fully signed offer to the wallet bootstrap.
+    /// Causes bob to send this spend bundle down the wire to the other peer.
+    ///
+    /// When these spend bundles are combined and deduplicated, together a
+    /// fully spendble transaction will result, to which fee might need to be
+    /// added.
+    ///
+    /// Alice sends this to the wallet interface via received_channel_transaction
+    /// completion to finish this phase of execution.
+    ///
+    /// Bob receives this callback from the wallet interface with the fully funded
+    /// but not fee adjusted spend bundle on bob's side.  It is given back to alice
+    /// and must contain appropriate spends to generate the launcher coin
+    /// announcement.
+    ///
+    /// This is sent back to alice as message F.
+    ///
+    /// Both alice and bob, upon knowing the full channel coin id, use the more
+    /// general wallet interface to register for notifications of the channel coin.
+    fn channel_transaction_completion(&mut self, bundle: &SpendBundle) -> Result<(), Error>;
 }
 
 /// Async interface implemented by Peer to receive notifications about wallet
 /// state.
 pub trait BootstrapTowardWallet {
     /// Deliver the channel_puzzle_hash to the wallet.
+    ///
+    /// Only alice calls this.  Bob does not need this information because the
+    /// information needed will be held at the level of the injected object instead.
     fn channel_puzzle_hash(&mut self, puzzle_hash: &PuzzleHash) -> Result<(), Error>;
 
-    /// Gives a partly signed offer to the wallet bootstrap.
-    fn channel_offer(&mut self, bundle: &TransactionBundle) -> Result<(), Error>;
-    /// Gives the fully signed offer to the wallet bootstrap.
-    fn channel_transaction_completion(&mut self, bundle: &TransactionBundle) -> Result<(), Error>;
+    /// Tells the game layer that we received a partly funded offer to which we
+    /// added our own coins and sent to the bootstrap wallet interface to use.
+    /// We had previously received a partly funded spend bundle via the reply to
+    /// channel_puzzle_hash,
+    /// Should add a fee and try to spend.
+    ///
+    /// Asynchronously, channel_transaction_completion is delivered back to the
+    /// potato handler.
+    ///
+    /// Only bob sends this, upon receiving message E, bob makes this call to
+    /// inform the injected wallet bootstrap dependency that the spend bundle
+    /// has been received (partly funded so far) and it is the job of the bootstrap
+    /// wallet object injected dependency to finish funding this and actually
+    /// spend it.
+    fn received_channel_offer(&mut self, bundle: &SpendBundle) -> Result<(), Error>;
+
+    /// Bob has sent this to us via the potato interface and it is given here to
+    /// the wallet injected dependency to actually spend.  Alice must add a fee
+    /// if needed.
+    ///
+    /// Both alice and bob, upon knowing the full channel coin id, use the more
+    /// general wallet interface to register for notifications of the channel coin.
+    fn received_channel_transaction_completion(&mut self, bundle: &SpendBundle) -> Result<(), Error>;
 }
 
 /// Spend wallet receiver
@@ -247,7 +328,6 @@ impl Peer {
         reward_puzzle_hash: PuzzleHash,
     ) -> Peer {
         Peer {
-            have_potato,
             handshake_state:
             if have_potato {
                 HandshakeState::PreInit
@@ -417,7 +497,6 @@ impl Peer {
                 };
 
                 // Factor out to one method.
-                self.have_potato = false;
                 penv.system_interface.send_message(&PeerMessage::HandshakeB(our_handshake_data))?;
             }
 
@@ -473,8 +552,30 @@ impl Peer {
                     second_player_hs_info: msg,
                 };
 
-                self.have_potato = false;
                 penv.system_interface.send_message(&PeerMessage::Nil(nil_sigs))?;
+            }
+
+            HandshakeState::StepD {
+                first_player_hs_info,
+                second_player_hs_info
+            } => {
+                self.pass_on_channel_handler_message(
+                    penv,
+                    msg
+                )?;
+                self.handshake_state = HandshakeState::StepF {
+                    first_player_hs_info: first_player_hs_info.clone(),
+                    second_player_hs_info: second_player_hs_info.clone()
+                };
+
+                // Outer layer already knows the launcher coin string.
+                //
+                // Provide the channel puzzle hash to the full node bootstrap and
+                // it replies with the channel puzzle hash
+                todo!();
+                // penv.system_interface.send_message(&PeerMessage::HandshakeE {
+                // bundle: 
+                // })?;
             }
 
             _ => {
