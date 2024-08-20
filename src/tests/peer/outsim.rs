fc9ce2070f436aa72ac6415b9ebb6c8d570b1f10
arty <art.yerkes@gmail.com>
2024-08-20 13:18:43 -0700

    fmt

--- src/tests/peer/outsim.rs
+++ src/tests/peer/outsim.rs
@@ -30,8 +30,8 @@ use crate::peer_container::{
 
 use crate::simulator::Simulator;
 use crate::tests::calpoker::test_moves_1;
-use crate::tests::peer::outside::{quiesce, run_move};
 use crate::tests::game::GameAction;
+use crate::tests::peer::outside::{quiesce, run_move};
 
 // potato handler tests with simulator.
 #[derive(Default)]
