1571243e57ce2600c1a25262f4d069edd63f6bb7
arty <art.yerkes@gmail.com>
2024-08-22 18:49:23 -0700

    Better display, getting turn order and data decoding better

--- resources/web/player.js
+++ resources/web/player.js
@@ -2,10 +2,13 @@ let all_selected_cards = {};
 let have_made_move_in_current_state = false;
 let most_recent_state = null;
 let latched_in_move_state = null;
+let made_move = false;
 let auto_moves = {
     "BeforeAliceWord": true,
     "WaitingForAliceWord": true
 };
+let label_by_rank = "0123456789JQKA";
+let label_by_suit = "Y\u{2660}\u{2665}\u{2663}\u{2666}";
 
 function clear(elt) {
     for (let node = elt.firstChild; node; node = elt.firstChild) {
@@ -63,7 +66,7 @@ function send_alice_word() {
 
 function send_bob_word() {
     let word = Math.random().toString();
-    return fetch(`bob_word?word=${word}`, {
+    return fetch(`bob_word_hash?word=${word}`, {
         "method": "POST"
     });
 }
@@ -100,18 +103,59 @@ function take_auto_action(player_id, json) {
 }
 
 function generate_alice_entropy(player_id) {
-    return fetch(`alice_word_hash?arg=${player_id}${Math.random()}`).then((response) => {
+    return fetch(`alice_word_hash?arg=${player_id}${Math.random()}`, {
+        "method": "POST"
+    }).then((response) => {
         return response.json();
     }).then((json) => {
         latched_in_move_state = null;
+        made_move = true;
     });
+}
+
+function generate_bob_entropy(player_id) {
+    return fetch(`bob_word_hash?arg=${player_id}${Math.random()}`, {
+        "method": "POST"
+    }).then((response) => {
+        return response.json();
+    }).then((json) => {
+        latched_in_move_state = null;
+        made_move = true;
+    });
+}
+
+function render_card(card) {
+    let card_span = document.createElement('span');
+    card_span.setAttribute('class','card');
+    card_span.innerHTML = `<div class='rank${card[0]} suit${card[1]}'><div class='card_top'>${label_by_suit[card[1]]}</div><div class='card_bot'>${label_by_rank[card[0]]}</div>`;
+    return card_span;
+}
+
+function make_card_row(div, cards) {
+    for (let ci = 0; ci < cards.length; ci++) {
+        div.appendChild(render_card(cards[ci]));
+    }
 }
 
 function allow_manual_move(player_id, move, json) {
     console.log(`allow manual move ${player_id} ${move} ${JSON.stringify(json)}`);
     let element = document.getElementById('playspace');
     if (move === 'BeforeAliceWord') {
-        element.innerHTML = '<h2>Alice must generate a secret value and send a hash commitment</h2><div><button onclick="generate_alice_entropy">Generate secret value</button>"';
+        element.innerHTML = `<h2>You must generate a secret value and send a hash commitment</h2><div><button onclick="generate_alice_entropy(${player_id})">Generate secret value</button>"`;
+    } else if (move === 'WaitingForAlicePicks') {
+        element.innerHTML = `<h2>You must generate a secret value and send part of a hash commitment</h2><div><button onclick="generate_bob_entropy(${player_id})">Generate secret value</button>"`;
+    } else if (move === 'BeforeAlicePicks') {
+        element.innerHTML = "<h2>You must choose four of these cards</h2><div id='card-choices'></div><h2>Your opponent is being shown these cards</h2><div id='opponent-choices'></div>";
+        let choices = document.getElementById('card-choices');
+        if (typeof(json.readable) !== 'string' && json.readable.length == 2) {
+            make_card_row(choices, json.readable[0]);
+        }
+        let opponent = document.getElementById('opponent-choices');
+        if (typeof(json.readable) !== 'string' && json.readable.length == 2) {
+            make_card_row(opponent, json.readable[1]);
+        }
+    } else {
+        element.innerHTML = `unhandled state ${move}`;
     }
 }
 
@@ -128,11 +172,15 @@ function take_update(player_id, auto, json) {
                 take_auto_action(player_id, json);
             } else {
                 latched_in_move_state = first_time_seeing_state;
+                made_move = false;
             }
         }
     }
 
-    if (latched_in_move_state) {
+    if (made_move || json.our_move.length) {
+        let element = document.getElementById('playspace');
+        element.innerHTML = "Sent move waiting for state change.";
+    } else if (latched_in_move_state && !made_move) {
         allow_manual_move(player_id, latched_in_move_state, json);
     }
 
