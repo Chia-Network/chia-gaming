f43e2796783ebfa39866b83950767ed607234bd9
arty <art.yerkes@gmail.com>
2024-09-18 12:08:29 -0700

    Merge-branch-20231004-debugging-handcalc-of-https-github.com-bramcohen-chia-gaming-into-20231004-debugging-handcalc
    original-commit:
      commit f4723baa4a7257d71586c259452d76c767c25960
      date Wed Oct 18 16:18:38 2023 -0700
      author Bram Cohen

--- test_handcalc.sh
+++ test_handcalc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-run -i . test_handcalc.clsp > test_handcalc.clvm && \
+time run -i . test_handcalc.clsp > test_handcalc.clvm && \
     opc test_handcalc.clvm > test_handcalc.hex && \
-    cldb -p -x test_handcalc.hex 80
+    cldb -y main.sym -p -x test_handcalc.hex 80
