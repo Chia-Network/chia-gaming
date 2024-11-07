3471399cd7028bcf58f74db666e2f11ba99c8641
Adam Kelly <338792+aqk@users.noreply.github.com>
2024-11-07 10:43:28 -0800

    move test utils to TS

---
+++ wasm/tests/src/lib/tests/common.ts
@@ -0,0 +1,10 @@
+
+export function to_hex_string(byteArray: Array<number>) {
+  return Array.from(byteArray, function(byte: number) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('')
+}
+
+export function foo() {
+    return "foo";
+}
