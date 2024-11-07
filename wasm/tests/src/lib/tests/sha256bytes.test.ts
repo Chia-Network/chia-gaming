fbdc4e6ba9418b74d5d0b02c541d160de078fa23
Adam Kelly <338792+aqk@users.noreply.github.com>
2024-11-07 10:37:16 -0800

    Use npm assert

--- wasm/tests/src/lib/tests/sha256bytes.test.ts
+++ wasm/tests/src/lib/tests/sha256bytes.test.ts
@@ -1,6 +1,6 @@
 import { init, sha256bytes } from '../../../../pkg/chia_gaming_wasm.js';
-
-import { assert, to_hex_string } from '../../../../tests/src/lib/tests/common.js';
+import { to_hex_string } from './common';
+import * as assert from 'assert';
 
 let utf8Encode = new TextEncoder();
 let b = utf8Encode.encode("abc");
@@ -10,5 +10,5 @@ it('hashes', async () => {
     let msg = 'hello.there.my.dear.friend';
     let hash = sha256bytes(msg);
     console.log(msg, hash);
-    assert( to_hex_string(hash) === "5272821c151fdd49f19cc58cf8833da5781c7478a36d500e8dc2364be39f8216" );
+    assert.equal( to_hex_string(hash), "5272821c151fdd49f19cc58cf8833da5781c7478a36d500e8dc2364be39f8216" );
 });
