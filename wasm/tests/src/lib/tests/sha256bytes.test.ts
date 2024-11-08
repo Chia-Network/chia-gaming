a6bf52e5ab59e57a3d53bfbb7c947774690b4d9d
Adam Kelly <338792+aqk@users.noreply.github.com>
2024-11-08 04:45:21 -0800

    Remove test code

--- wasm/tests/src/lib/tests/sha256bytes.test.ts
+++ wasm/tests/src/lib/tests/sha256bytes.test.ts
@@ -10,6 +10,5 @@ it('hashes', async () => {
     let msg = 'hello.there.my.dear.friend';
     let hash = sha256bytes(msg);
     console.log(msg, hash);
-    console.log(foo());
     assert.equal( to_hex_string(hash), "5272821c151fdd49f19cc58cf8833da5781c7478a36d500e8dc2364be39f8216" );
 });
