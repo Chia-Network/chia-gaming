7d83796551906391183c293bedd87c27ff429ea7
David Kim <dckim320@gmail.com>
2024-11-26 12:42:42 -0800

    Set up scaffolding for cal poker FE

---
+++ resources/gaming-fe/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Game from './Game'
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
