db23c07ab523e690e38431de08b32b139cde3807
arty <art.yerkes@gmail.com>
2025-04-15 16:55:37 -0700

    Python works but doesn't do quite what's wanted: we want to manipulate selects rather than picks

--- pytests/find_test_hands.py
+++ pytests/find_test_hands.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 from hashlib import sha256
 from pathlib import Path
 from typing import List
+from itertools import permutations
 
 from clvm_types.program import Program
 
@@ -28,9 +29,8 @@ class Hand:
 
     def __init__(self, card_list):
         self.cards = card_list
-
     def to_clvm(self):
-        print("self.cards", self.cards)
+        # print("self.cards", self.cards)
         return Program.to([(card.rank, card.suit) for card in self.cards])
 
 
@@ -126,8 +126,9 @@ def onehandcalc(hand: Hand):
 
 def handcalc(hand: Hand):
     result = test_handcalc_micro.run(["handcalc", hand.to_clvm()])
-    print('handcalc', result.as_python())
-    return result.as_python()
+    py_result = result.as_python()
+    # print('handcalc', py_result)
+    return (py_result[0], [Program.to(x).as_int() for x in py_result[1]])
 
 def int_to_clvm_bytes(i: int) -> bytes:
     return bytes(Program.to(i))
@@ -168,24 +169,59 @@ def clvm_byte_to_int(b: bytes) -> int:
     return ord(b)
 
 
+def cards_for_picks(cards, picks):
+    """Cards is a list of cards, picks is a list of indices"""
+    return ([card for (i,card) in enumerate(cards) if i in picks], [card for (i,card) in enumerate(cards) if i not in picks])
+
+
+
+
 def exchange_cards(alice_hand, bob_hand, alice_picks, bob_picks):
-    alice_give_away = [card for (i,card) in enumerate(alice_hand) if i in alice_picks]
-    bob_give_away = [card for (i,card) in enumerate(bob_hand) if i in bob_picks]
-    alice_keep = [card for (i,card) in enumerate(alice_hand) if i not in alice_picks]
-    bob_keep = [card for (i,card) in enumerate(bob_hand) if i not in bob_picks]
+    alice_give_away, alice_keep = cards_for_picks(alice_hand, alice_picks)
+    bob_give_away, bob_keep = cards_for_picks(bob_hand, bob_picks)
     return (alice_keep + bob_give_away, bob_keep + alice_give_away)
 
+
+def find_win_and_loss(alice_initial_hand, bob_initial_hand):
+    potential_choices = list(range(8))
+    found_alice_win = None
+    found_bob_win = None
+    tried = set()
+
+    for full_alice_choice in permutations(potential_choices):
+        alice_picks = full_alice_choice[:4]
+
+        bob_picks = [choice for choice in potential_choices if choice not in alice_picks]
+        alice_final_cards, bob_final_cards = exchange_cards(alice_initial_hand, bob_initial_hand, alice_picks, bob_picks)
+        alice_handcalc = handcalc(Hand(alice_final_cards))
+        bob_handcalc = handcalc(Hand(bob_final_cards))
+        alice_picked_hand = cards_for_picks(alice_final_cards, alice_handcalc[1])[0]
+        bob_picked_hand = cards_for_picks(bob_final_cards, bob_handcalc[1])[0]
+        alice_hand_rating = onehandcalc(Hand(alice_picked_hand))
+        bob_hand_rating = onehandcalc(Hand(bob_picked_hand))
+        print(f"checking picks {alice_picks} alice rating {alice_hand_rating} bob {bob_hand_rating}")
+        if alice_hand_rating < bob_hand_rating:
+            found_alice_win = (alice_picks, bob_picks)
+        if bob_hand_rating > bob_hand_rating:
+            found_bob_win = (alice_picks, bob_picks)
+
+        if found_alice_win is not None and found_bob_win is not None:
+            return (found_alice_win, found_bob_win)
+
+    return None
 
 # An example of a good hand:
 # good_hand = Hand([Card(14, 1), Card(14, 2), Card(14, 3), Card(14, 4), Card(2, 2)])
 # good_hand_rating = onehandcalc(good_hand)
 # print(f"good_hand_rating: {good_hand_rating}")
 def find_tie():
-    int_seed = 0
+    int_seed = 460
     alice_hand_rating = 0
     bob_hand_rating = 1
+    can_make_win_and_loss = None
 
-    while alice_hand_rating != bob_hand_rating:
+    while (can_make_win_and_loss is None) and alice_hand_rating != bob_hand_rating:
+        int_seed += 1
         alice_seed = sha256(("alice" + str(int_seed)).encode("utf8")).digest()
         bob_seed = sha256(("bob" + str(int_seed)).encode("utf8")).digest()
         seed = sha256(alice_seed + bob_seed + bytes(Program.to(200))).digest()
@@ -197,14 +233,28 @@ def find_tie():
         alice_final_cards, bob_final_cards = exchange_cards(alice_initial_hand, bob_initial_hand, alice_picks, bob_picks)
         alice_handcalc = handcalc(Hand(alice_final_cards))
         bob_handcalc = handcalc(Hand(bob_final_cards))
-        alice_picked_hand = [card for (i,card) in enumerate(alice_final_cards) if Program.to(i) in alice_handcalc[1]]
-        bob_picked_hand = [card for (i,card) in enumerate(bob_final_cards) if Program.to(i) in bob_handcalc[1]]
+        alice_picked_hand = cards_for_picks(alice_final_cards, alice_handcalc[1])[0]
+        bob_picked_hand = cards_for_picks(bob_final_cards, bob_handcalc[1])[0]
+
         alice_hand_rating = onehandcalc(Hand(alice_picked_hand))
         bob_hand_rating = onehandcalc(Hand(bob_picked_hand))
-        int_seed += 1
 
-    print(f"Tie found. int_seed={int_seed}")
+        if alice_hand_rating != bob_hand_rating:
+            print(f"{int_seed} alice and bob didn't tie on tie picks")
+            continue
+
+        can_make_win_and_loss = find_win_and_loss(alice_initial_hand, bob_initial_hand)
+        if not can_make_win_and_loss:
+            print(f"{int_seed} can't make win and loss")
+            # Can't make win and loss so this isn't a suitable seed
+            alice_hand_rating = None
+
+    print(f"\n\n***\n\nTie found. int_seed={int_seed}")
     print("Alice hand:", alice_picked_hand)
+    print(f"Alice win picks:", can_make_win_and_loss[0])
+    print(f"Alice win cards:", cards_for_picks(alice_final_cards, can_make_win_and_loss[0][0])[0])
+    print(f"Bob win picks:", can_make_win_and_loss[1])
+    printf("Bob win cards:", cards_for_picks(bob_final_cards, can_make_win_and_loss[1][1])[0])
     print("  Bob hand:", [CardIndex(clvm_byte_to_int(i)) for i in r[1]])
 
 
