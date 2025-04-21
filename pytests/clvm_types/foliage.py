375caf8ea9797f8e28dcf6e311ae06a9b4e423e1
arty <art.yerkes@gmail.com>
2025-04-21 16:38:46 -0700

    re-add

---
+++ pytests/clvm_types/foliage.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+import chia_rs
+
+TransactionsInfo = chia_rs.TransactionsInfo
+FoliageTransactionBlock = chia_rs.FoliageTransactionBlock
+FoliageBlockData = chia_rs.FoliageBlockData
+Foliage = chia_rs.Foliage
