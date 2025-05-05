edb7cfe6fa5a9bc84665f00a473bd8fb03e09f7c
arty <art.yerkes@gmail.com>
2025-05-05 12:22:13 -0700

    opps

--- src/tests/peer/potato_handler.rs
+++ src/tests/peer/potato_handler.rs
@@ -401,7 +401,7 @@ fn test_peer_smoke() {
     let mut game_type_map = BTreeMap::new();
     let calpoker_factory = read_hex_puzzle(
         &mut allocator,
-        "clsp/games/calpoker-v0/calpoker_include_calpoker_factory_hash.hex",
+        "clsp/games/calpoker-v0/calpoker_include_calpoker_factory.hex",
     )
     .expect("should load");
 
