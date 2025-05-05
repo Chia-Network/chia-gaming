bf6d4b9bca08cc775a0448f38750827f4e1dcd9f
arty <art.yerkes@gmail.com>
2025-05-05 10:52:12 -0700

    Calling factory

--- src/tests/debug_game.rs
+++ src/tests/debug_game.rs
@@ -4,22 +4,47 @@ use std::collections::VecDeque;
 use rand_chacha::ChaCha8Rng;
 use rand::prelude::*;
 
-use clvm_traits::{ToClvm, clvm_curried_args};
+use clvmr::NodePtr;
+
+use clvm_traits::{ClvmEncoder, ToClvm, ToClvmError, clvm_curried_args};
 use clvm_utils::CurriedProgram;
 
 use crate::channel_handler::game::Game;
 use crate::channel_handler::game_handler::GameHandler;
 use crate::channel_handler::types::{GameStartInfo, StateUpdateProgram, ValidationInfo};
-use crate::common::types::{AllocEncoder, Amount, Error, GameID, Hash, IntoErr, PrivateKey, Program, Puzzle, PuzzleHash, Sha256tree, Timeout};
+use crate::common::types::{AllocEncoder, Amount, Error, GameID, Hash, IntoErr, PrivateKey, Program, ProgramRef, Puzzle, PuzzleHash, Sha256tree, Timeout};
 use crate::common::standard_coin::{ChiaIdentity, read_hex_puzzle};
 use crate::referee::types::{GameMoveDetails, GameMoveStateInfo, RefereePuzzleArgs};
 
 pub struct DebugGameCurry {
-    mode: String,
     count: usize,
+    self_hash: PuzzleHash,
     self_prog: Rc<Program>,
     mover0: PuzzleHash,
     waiter0: PuzzleHash,
+}
+
+impl<E: ClvmEncoder<Node = NodePtr>> ToClvm<E> for DebugGameCurry
+where
+    NodePtr: ToClvm<E>
+{
+    fn to_clvm(&self, encoder: &mut E) -> Result<<E as ClvmEncoder>::Node, ToClvmError> {
+        (
+            self.count,
+            (
+                self.self_hash.clone(),
+                (
+                    self.self_prog.clone(),
+                    (
+                        self.mover0.clone(),
+                        (
+                            (self.waiter0.clone(), ())
+                        )
+                    )
+                )
+            )
+        ).to_clvm(encoder)
+    }
 }
 
 pub struct DebugGame {
@@ -61,32 +86,17 @@ impl BareDebugGameDriver {
         game_hex_file: &str,
     ) -> Result<[BareDebugGameDriver; 2], Error> {
         let raw_program = read_hex_puzzle(allocator, game_hex_file)?;
-        let referee_args = RefereePuzzleArgs {
-            amount,
-            nonce: 0,
-            max_move_size: 256,
-            mover_puzzle_hash: identities[0].puzzle_hash.clone(),
-            waiter_puzzle_hash: identities[1].puzzle_hash.clone(),
-            timeout: Timeout::new(10),
-            game_move: GameMoveDetails {
-                basic: GameMoveStateInfo {
-                    move_made: vec![],
-                    mover_share: Amount::default(),
-                },
-                validation_info_hash: Hash::default(),
-            },
-            previous_validation_info_hash: None,
-            referee_coin_puzzle_hash: referee_coin_puzzle_hash.clone(),
+        let prog_hash = raw_program.sha256tree(allocator);
+        let args = DebugGameCurry {
+            count: 0,
+            self_prog: raw_program.to_program(),
+            self_hash: prog_hash,
+            mover0: identities[0].puzzle_hash.clone(),
+            waiter0: identities[1].puzzle_hash.clone()
         };
         let curried = CurriedProgram {
             program: raw_program.to_clvm(allocator).into_gen()?,
-            args: clvm_curried_args!((
-                "factory",
-                (
-                    referee_args,
-                    ()
-                )
-            ))
+            args: clvm_curried_args!("factory", args)
         }.to_clvm(allocator).into_gen()?;
         let curried_prog = Program::from_nodeptr(allocator, curried)?;
         let alice_game = Game::new_program(allocator, true, game_id.clone(), curried_prog.clone().into())?;
