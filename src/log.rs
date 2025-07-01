3b27583e726117f0ce730bb343e441eff556c888
arty <art.yerkes@gmail.com>
2025-07-01 00:22:21 -0700

    fmt

--- src/log.rs
+++ src/log.rs
@@ -13,10 +13,12 @@ pub fn init<W: io::Write + Send + 'static>(writer: W) {
 #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
 #[ctor::ctor]
 fn log_init() {
-    let rust_log = std::env::var("RUST_LOG").map(|v| v.to_string()).unwrap_or_else(|_| "0".to_string());
+    let rust_log = std::env::var("RUST_LOG")
+        .map(|v| v.to_string())
+        .unwrap_or_else(|_| "0".to_string());
     let filter = if rust_log == "debug" {
         LevelFilter::Debug
-    }  else {
+    } else {
         LevelFilter::Error
     };
     let _ = TermLogger::init(
