f478b0daef2b065440b194f285a230d8f0d80a3f
arty <art.yerkes@gmail.com>
2025-07-01 08:17:33 -0700

    now takes hex

--- wasm/tests/src/lib/tests/sha256bytes.test.ts
+++ wasm/tests/src/lib/tests/sha256bytes.test.ts
@@ -7,7 +7,7 @@ let b = utf8Encode.encode("abc");
 
 it('hashes', async () => {
     init();
-    let msg = 'hello.there.my.dear.friend';
+    let msg = '68656c6c6f2e74686572652e6d792e646561722e667269656e64'; // 'hello.there.my.dear.friend'
     let hash = sha256bytes(msg);
     console.log(msg, hash);
     assert.equal( to_hex_string(hash), "5272821c151fdd49f19cc58cf8833da5781c7478a36d500e8dc2364be39f8216" );
