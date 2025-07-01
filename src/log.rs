c4c4fed41b5c82cb83c87617d861e42272191289
arty <art.yerkes@gmail.com>
2025-07-01 00:17:50 -0700

    restore env debug switch for logging

--- src/log.rs
+++ src/log.rs
@@ -13,8 +13,14 @@ pub fn init<W: io::Write + Send + 'static>(writer: W) {
 #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
 #[ctor::ctor]
 fn log_init() {
+    let rust_log = std::env::var("RUST_LOG").map(|v| v.to_string()).unwrap_or_else(|_| "0".to_string());
+    let filter = if rust_log == "debug" {
+        LevelFilter::Debug
+    }  else {
+        LevelFilter::Error
+    };
     let _ = TermLogger::init(
-        LevelFilter::Debug,
+        filter,
         Config::default(),
         TerminalMode::Mixed,
         ColorChoice::Auto,
