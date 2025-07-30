b183d36524c7c9a54be5bb7bec424aced450a0ad
Adam Kelly <338792+aqk@users.noreply.github.com>
2025-07-30 15:34:20 -0700

    Add dev build scripts for local repeatable builds

--- clean-all.sh
+++ clean-all.sh
@@ -2,6 +2,8 @@
 rm -rf ./resources/gaming-fe/node_modules
 rm -rf ./wasm/node_modules
 rm -rf ./wasm/tests/node_modules
+rm -rf ./wasm/target
+rm -rf ./target
 
 (cd wasm && cargo clean)
 cargo clean
