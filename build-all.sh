7d2e762bbf3623825417fc28b9965f6a7cc70f5d
Adam Kelly <338792+aqk@users.noreply.github.com>
2025-08-07 22:44:28 -0700

    make build script more completex

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -x
 ./clean-all.sh
 
 (cd wasm && cargo build)
 (cd ./resources/gaming-fe && yarn install)
+
+. ./venv/bin/activate
+maturin build --features=simulator
+pip uninstall chia-gaming
+pip install `find ./target -name \*.whl`
+
 ./build-docker-images.sh
-
-
