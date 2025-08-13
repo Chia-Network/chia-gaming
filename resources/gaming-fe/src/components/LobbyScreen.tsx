c7d217605f0bc838628cd82460971ca3f132fbd8
Adam Kelly <338792+aqk@users.noreply.github.com>
2025-08-13 08:01:08 -0700

    Add change alias UX

--- resources/gaming-fe/src/components/LobbyScreen.tsx
+++ resources/gaming-fe/src/components/LobbyScreen.tsx
@@ -90,7 +90,7 @@ const LobbyScreen: React.FC<LobbyComponentProps> = ({ walletConnect }) => {
             borderRadius: '5px',
             padding: '8px 16px',
           }}
-        onClick={() => setEditingAlias(true)} >Your Alias: {myAlias}</span>
+        onClick={() => setEditingAlias(true)} >{myAlias}</span>
       );
   };
 
@@ -100,7 +100,7 @@ const LobbyScreen: React.FC<LobbyComponentProps> = ({ walletConnect }) => {
       <Typography variant="h4" gutterBottom>
         Chia Gaming Lobby
       </Typography>
-      <Typography>{aliasDisplay}</Typography>
+      <Typography>Your Alias: {aliasDisplay}</Typography>
       </Box>
       <span style={{ width: '0px', height: '0px', overflow: 'hidden', position: 'relative' }} aria-label="partner-target-url">{gotoUrl}</span>
       <Box mb={3}>
