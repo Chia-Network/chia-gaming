57302faa22da980e5bb4d910af7c1a6cfbc668f9
arty <art.yerkes@gmail.com>
2025-08-18 12:22:39 -0700

    WIP

--- resources/fe-test/test2/src/test.js
+++ resources/fe-test/test2/src/test.js
@@ -3,28 +3,24 @@ const process = require('process');
 const jasmine = require('jasmine');
 const os = require('os');
 const fs = require('fs');
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const geckodriver = require('geckodriver');
+const webdriver = require('selenium-webdriver');
 const {wait, byExactText, byAttribute, byElementAndAttribute, sendEnter, waitEnabled, selectSimulator} = require('../../test1/src/util.js');
+const {Builder, Browser, By, Key, Service, until} = webdriver;
+const wdio = require('webdriverio');
+const waitPort = require('wait-port');
 
 const baseUrl = fs.readFileSync('base.url').toString('utf8');
 console.log('starting with url', baseUrl);
 
-const options2 = new chrome.Options();
-options2.addArguments('--incognito');
-options2.addArguments('--remote-debugging-port=9223');
-
-const driver = new Builder()
-      .forBrowser(Browser.CHROME)
-      .setChromeOptions(options2)
-      .usingPort(9223)
-      .build();
-
 // Define a category of tests using test framework, in this case Jasmine
 describe("Basic element tests", function() {
   it("starts", async function() {
-    // Load the login page
-    await driver.get(baseUrl);
+    const gecko_process = await geckodriver.start({ port: 4444 });
+    await waitPort({ port: 4444 });
+
+    const driver = await wdio.remote({ capabilities: { browserName: 'firefox' } });
+    await driver.url(baseUrl);
 
     // Select simulator
     console.log('select simulator');
