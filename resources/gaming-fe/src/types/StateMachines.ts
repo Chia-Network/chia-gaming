16a7e79c80886ae3364ffd5417cd6b031421398b
Adam Kelly <338792+aqk@users.noreply.github.com>
2025-08-25 15:27:45 -0700

    Add missing file

---
+++ resources/gaming-fe/src/types/StateMachines.ts
@@ -0,0 +1,2 @@
+export type AppModeType = "disconnected" | "app_has_lobby_subframe" | "app_has_game_subframe" | "app_is_game_subframe" | "app_is_lobby_subframe";
+export type ExternalApiType = "simulator" | "walletconnect" | "unconnected";
