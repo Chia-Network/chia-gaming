ec1df873be026498ed4cde75ac1fdfef113e6425
Adam Kelly <338792+aqk@users.noreply.github.com>
2025-10-02 19:34:27 -0700

    Improve selenium test on mac

--- resources/fe-test/test.sh
+++ resources/fe-test/test.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+set -x
 set -e
 
 yarn install --dev
 
 docker kill chia-gaming-test || true
 docker rm chia-gaming-test || true
-docker run --name chia-gaming-test --network=host -t chia-gaming-test &
+docker run --name chia-gaming-test -p 127.0.0.1:3000:3000 -p 127.0.0.1:3001:3001 -p 127.0.0.1:5800:5800  -t chia-gaming-test &
+
+if [ -z "$FIREFOX" ]; then
+  case $(uname) in
+  Darwin)
+    export FIREFOX=/Applications/Firefox.app/Contents/MacOS
+    sleep 13
+    ;;
+  *)
+    echo "Please set env var 'FIREFOX'";;
+  esac
+else
+  echo "Using env var FIREFOX=${FIREFOX}"
+fi
 
 echo 'waiting for service alive .'
 /bin/bash ./wait-for-it.sh -t 90 -h localhost -p 3000
