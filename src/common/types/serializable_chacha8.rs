790d6cb9974beacbb3f1f644d819cc13f7f10da8
Adam Kelly <338792+aqk@users.noreply.github.com>
2025-10-20 15:46:00 -0700

    make rng serializable

---
+++ src/common/types/serializable_chacha8.rs
@@ -0,0 +1,30 @@
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+pub struct ChaCha8SerializationWrapper(pub ChaCha8Rng);
+
+impl Serialize for ChaCha8SerializationWrapper {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        hex::encode(self.0.get_seed()).serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for ChaCha8SerializationWrapper {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let st = String::deserialize(deserializer)?;
+        let slice = hex::decode(&st).unwrap();
+
+        let mut bytes: [u8; 32] = [0; 32];
+        for (i, b) in slice.iter().enumerate() {
+            bytes[i % 32] = *b;
+        }
+        Ok(ChaCha8SerializationWrapper(ChaCha8Rng::from_seed(bytes)))
+    }
+}
