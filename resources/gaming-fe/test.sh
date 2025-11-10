a6eb72eb4a6803092a9bfcbaaddc619a8fa7a681
arty <art.yerkes@gmail.com>
2025-11-10 11:00:57 -0800

    change

--- resources/gaming-fe/test.sh
+++ resources/gaming-fe/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 cd /app/game
-(. /app/test/bin/activate && python3 /app/run_simulator.py) &
+(cd /app && . /app/test/bin/activate && python3 ./run_simulator.py) &
 sleep 5
 yarn test
 
