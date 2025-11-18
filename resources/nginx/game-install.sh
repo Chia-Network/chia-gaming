15d823be745c69c72c5f15f0e5d396913f7ae8a0
arty <art.yerkes@gmail.com>
2025-11-18 10:21:03 -0800

    Add ports (WIP)

--- resources/nginx/game-install.sh
+++ resources/nginx/game-install.sh
@@ -7,9 +7,10 @@ WEBROOT=/usr/share/nginx/html/chia-gaming-game
 SERVICE=""
 TRACKER=""
 SELF=""
+PORT=""
 
 if [ "x$1" = x ] ; then
-	echo "usage: game-install.sh --nginx [nginx-sites-dir] --content-root [server-root] --self http://myself.com --tracker http://tracker.com --service [dir]"
+	echo "usage: game-install.sh --nginx [nginx-sites-dir] --content-root [server-root] --self http://myself.com --tracker http://tracker.com --service [dir] --port [port]"
 	exit 1
 fi
 
@@ -40,6 +41,11 @@ while [ "x$1" != x ] ; do
 			SERVICE="$1"
 			;;
 
+    x--port)
+      shift
+      PORT="$1"
+      ;;
+
 		*)
 			echo "Unknown argument $1"
 			exit 1
@@ -58,13 +64,37 @@ if [ "x$SELF" = x ] ; then
 	exit 1
 fi
 
+if [ "x$PORT" = x ] ; then
+    PROTO=$(trurl --get "{scheme}" "${SELF}")
+    URL_PORT=$(trurl --get "{port}" "${SELF}")
+
+    if [ "x${URL_PORT}" = x ] ; then
+        case "x${PROTO}" in
+            xhttp)
+                PORT=80
+                ;;
+
+            xhttps)
+                PORT=443
+                ;;
+
+            *)
+                echo "Unknown self url scheme in ${SELF}, specify https, http or --port"
+                exit 1
+                ;;
+        esac
+    else
+        PORT="${URL_PORT}"
+    fi
+fi
+
 mkdir -p "${WEBROOT}"
 mkdir -p "${NGINX}"
 mkdir -p "${SERVICE}"
 
 TRACKER_WS="$(echo "${TRACKER}" | sed -e 's/^http/^ws/g')"
 
-sed -e "s@/app@${WEBROOT}@g" -e "s!http://localhost:3001!${TRACKER}!g" -e "s!ws://localhost:3001!${TRACKER_WS}!g" < ./nginx/game.conf > "${NGINX}/game.conf"
+sed -e "s@/app@${WEBROOT}@g" -e "s!http://localhost:3001!${TRACKER}!g" -e "s!ws://localhost:3001!${TRACKER_WS}!g" -e "s!@PORT@!${PORT}!g" < ./nginx/game.conf > "${NGINX}/game.conf"
 
 # Install beacon service if we're on a systemd system
 if [ -d /etc/systemd/system ] ; then
