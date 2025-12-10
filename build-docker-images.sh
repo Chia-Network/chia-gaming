0a8bc6de1c9858f45689e9737284b4c064fbbe1a
Adam Kelly <338792+aqk@users.noreply.github.com>
2025-12-09 18:21:14 -0800

    return failure exit status if docker build fails

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
 # Build Chia Gaming Docker image
-docker build --platform linux/amd64 --progress=plain -t chia-gaming-test .
+exec docker build --platform linux/amd64 --progress=plain -t chia-gaming-test .
 
 # Build Lobby Service Docker image
-(cd ./resources/gaming-fe/src/lobby; ls)
+# (cd ./resources/gaming-fe/src/lobby; ls)
 
