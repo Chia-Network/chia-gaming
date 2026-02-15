16295a05140e53d82874de075145e8e54340f5b6
Adam Kelly <338792+aqk@users.noreply.github.com>
2026-02-15 04:44:43 -0800

    build chialisp .hex files

---
+++ build.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+use std::fs;
+
+use clvmr::Allocator;
+use toml::{Table, Value};
+
+use chialisp::classic::clvm_tools::clvmc::CompileError;
+use chialisp::classic::clvm_tools::comp_input::RunAndCompileInputData;
+use chialisp::classic::platform::argparse::ArgumentValue;
+use chialisp::compiler::comptypes::CompileErr;
+use chialisp::compiler::srcloc::Srcloc;
+
+fn do_compile(title: &str, filename: &str) -> Result<(), CompileError> {
+    let mut allocator = Allocator::new();
+    let mut arguments: HashMap<String, ArgumentValue> = HashMap::new();
+    arguments.insert(
+        "include".to_string(),
+        ArgumentValue::ArgArray(vec![
+            ArgumentValue::ArgString(None, "clsp".to_string()),
+            ArgumentValue::ArgString(None, ".".to_string()),
+        ]),
+    );
+
+    let file_content = fs::read_to_string(filename).map_err(|e| {
+        CompileErr(
+            Srcloc::start(filename),
+            format!("failed to read {filename}: {e:?}"),
+        )
+    })?;
+
+    arguments.insert(
+        "path_or_code".to_string(),
+        ArgumentValue::ArgString(Some(filename.to_string()), file_content),
+    );
+
+    let parsed = RunAndCompileInputData::new(&mut allocator, &arguments).map_err(|e| {
+        CompileError::Modern(
+            Srcloc::start("*error*"),
+            format!("error building chialisp {title}: {e}"),
+        )
+    })?;
+    let mut symbol_table = HashMap::new();
+
+    parsed.compile_modern(&mut allocator, &mut symbol_table)?;
+
+    Ok(())
+}
+
+fn compile_chialisp() -> Result<(), CompileError> {
+    let srcloc = Srcloc::start("chialisp.toml");
+    let chialisp_toml_text = fs::read_to_string("chialisp.toml").map_err(|e| {
+        CompileError::Modern(
+            srcloc.clone(),
+            format!("Error reading chialisp.toml: {e:?}"),
+        )
+    })?;
+
+    let chialisp_toml = chialisp_toml_text
+        .parse::<Table>()
+        .map_err(|e| CompileError::Modern(srcloc, format!("Error parsing chialisp.toml: {e:?}")))?;
+
+    if let Some(Value::Table(t)) = chialisp_toml.get("compile") {
+        for (k, v) in t.iter() {
+            if let Value::String(s) = v {
+                do_compile(k, s)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// Compile chialisp programs in this tree.
+fn main() {
+    if std::env::var("CHIALISP_NOCOMPILE").is_err() {
+        if let Err(e) = compile_chialisp() {
+            panic!("error compiling chialisp: {e:?}");
+        }
+    }
+}
