933ac9af3542136621d4f0814b4f48c8be5f4405
Bram Cohen <bram@chia.net>
2026-02-27 00:35:20 -0800

    Removed Python stuff

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,37 +1,6 @@
-use exec::execvp;
-use std::ffi::OsString;
-
 pub mod calpoker_handlers;
 pub mod calpoker_validation;
 pub mod channel_handler;
 pub mod chialisp;
 pub mod constants;
 pub mod standard_coin;
-
-fn detect_run_as_python(args: &[String]) -> bool {
-    args.iter().any(|x: &String| x == "-c")
-}
-
-// Catch attempts on macos to run the test rig as 'python' and run python code in it.
-#[ctor::ctor]
-fn init() {
-    let args = std::env::args();
-    let args_vec: Vec<String> = args.collect();
-    if detect_run_as_python(&args_vec) {
-        let new_args: Vec<OsString> = args_vec
-            .iter()
-            .enumerate()
-            .map(
-                |(i, arg)| {
-                    if i == 0 {
-                        "python3".into()
-                    } else {
-                        arg.into()
-                    }
-                },
-            )
-            .collect();
-        let exec_err = execvp("python3", &new_args);
-        eprintln!("Error Running: {:?}\n{:?}\n", new_args, exec_err);
-    }
-}
