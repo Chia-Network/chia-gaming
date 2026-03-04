79ca14807e6119924b551cf2e38089e91ad6ce03
Adam Kelly <338792+aqk@users.noreply.github.com>
2026-03-03 18:43:32 -0800

    add missing file

---
+++ src/channel_handler/types/proposed_game.rs
@@ -0,0 +1,33 @@
+use std::rc::Rc;
+
+use serde::{Deserialize, Serialize};
+
+use crate::common::types::{Amount, GameID, PuzzleHash};
+use crate::referee::Referee;
+
+#[derive(Serialize, Deserialize)]
+pub struct ProposedGame {
+    pub game_id: GameID,
+    pub initial_puzzle_hash: PuzzleHash,
+    pub referee: Rc<Referee>,
+    pub my_contribution: Amount,
+    pub their_contribution: Amount,
+}
+
+impl ProposedGame {
+    pub fn new(
+        game_id: GameID,
+        initial_puzzle_hash: PuzzleHash,
+        referee: Rc<Referee>,
+        my_contribution: Amount,
+        their_contribution: Amount,
+    ) -> Self {
+        ProposedGame {
+            game_id,
+            initial_puzzle_hash,
+            referee,
+            my_contribution,
+            their_contribution,
+        }
+    }
+}
