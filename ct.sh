e1d76e7b2fee750107ee4c64825428be167b0c05
Adam Kelly <338792+aqk@users.noreply.github.com>
2026-03-06 18:12:59 -0800

    improve tests

--- ct.sh
+++ ct.sh
@@ -3,21 +3,22 @@ set -e
 SECONDS=0
 trap 'echo "Total time: ${SECONDS} seconds"' EXIT
 
-echo "=== Building rust ==="
-rm -f build.rs
-cargo build --features sim-tests
-echo "Build took: ${SECONDS} seconds"
-
-echo "=== Building chialisp ==="
-./tools/build-chialisp.sh
-
 if [ "$1" = "-o" ] && [ -n "$2" ]; then
     export SIM_TEST_ONLY="$2"
 elif [ -n "$1" ]; then
     export SIM_TEST_FROM="$1"
 fi
+
+echo "=== Building rust ==="
+rm -f build.rs
+cargo build --features sim-tests
+echo "Build took: ${SECONDS} seconds"
+
 echo "=== Running rust tests ==="
 cargo test --lib --features sim-tests -- --nocapture
+
+echo "=== Building chialisp ==="
+./tools/build-chialisp.sh
 
 echo "=== Running JS tests ==="
 ./run-js-tests.sh
