eaddcaf42349e822f1c99314af255bab66445550
Matthew Howard <m.o.howard97@gmail.com>
2026-04-03 17:07:08 +0100

    fix window

--- front-end/src/settings.ts
+++ front-end/src/settings.ts
@@ -1,8 +1,9 @@
 const _host = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
+const _win = typeof window !== 'undefined' ? (window as any) : {};
 export const BLOCKCHAIN_SERVICE_URL =
-  (window as any).__SIM_URL__ || `http://${_host}:5800`;
+  _win.__SIM_URL__ || `http://${_host}:5800`;
 export const BLOCKCHAIN_WS_URL =
-  (window as any).__SIM_WS_URL__ || `ws://${_host}:5801`;
+  _win.__SIM_WS_URL__ || `ws://${_host}:5801`;
 export const GAME_SERVICE_URL =
-  (window as any).__GAME_URL__ || `http://${_host}:3002`;
+  _win.__GAME_URL__ || `http://${_host}:3002`;
 // Note: The Lobby URL is obtained from the "start game" URL
