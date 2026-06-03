0b365b706e8467251a2e74a2edf75461c9e52c9b
Matt Howard <m.o.howard97@gmail.com>
2026-06-03 15:34:18 +0100

    fix bigint expectations

--- front-end/src/types/rpc/GetFullNodePeerCount.ts
+++ front-end/src/types/rpc/GetFullNodePeerCount.ts
@@ -1,5 +1,3 @@
 export type GetFullNodePeerCountRequest = Record<string, never>;
 
-export interface GetFullNodePeerCountResponse {
-  peerCount: bigint;
-}
+export type GetFullNodePeerCountResponse = bigint;
